e721c841979d1809682afef847b4b61be6b61e72
Zebadiah <zebadiahww@gmail.com>
2020-05-20 10:40:48 -0600

    model created, model controller started

---
+++ Journal33Git/Journal33Git/Model Controller/EntryController.swift
@@ -0,0 +1,31 @@
+//
+//  EntryController.swift
+//  Journal33Git
+//
+//  Created by Zebadiah Watson on 5/20/20.
+//  Copyright © 2020 Zebadiah Watson. All rights reserved.
+//
+
+import Foundation
+
+
+class EntryController {
+    
+    static let sharedInstance = EntryController()
+    
+    var entries: [Entry] = []
+    
+    
+    //TODO - fill out method logic
+    func create() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+}
---
+++ Journal33Git/Journal33Git/Model/Entry.swift
@@ -0,0 +1,20 @@
+//
+//  Entry.swift
+//  Journal33Git
+//
+//  Created by Zebadiah Watson on 5/20/20.
+//  Copyright © 2020 Zebadiah Watson. All rights reserved.
+//
+
+import Foundation
+
+class Entry {
+    
+    var title: String
+    var body: String
+    
+    init(title: String, body: String) {
+        self.title = title
+        self.body = body
+    }
+}
